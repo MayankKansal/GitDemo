df608f34eeea306c3dc8b07a626453d1cc6ba1a0
martin <martin@123789.org>
2019-10-01 11:29:42 +0530

    from martin

--- src/ConstructorDemo/Main1.java
+++ src/ConstructorDemo/Main1.java
@@ -1,8 +1,13 @@
 package ConstructorDemo;
 
-final class Complex { 
+ class Complex { 
     private  double re,  im; 
+    int ar[] = new int[] {13};
+    
+   
+    
     public Complex(double re, double im) { 
+    	
         this.re = re; 
         this.im = im; 
     } 
@@ -16,10 +21,15 @@ final class Complex {
         return "(" + re + " + " + im + "i)"; 
     }             
 } 
-class Main1 { 
-    public static void main(String[] args) { 
+class Main1 extends Complex{ 
+    Main1(Complex c) {
+		super(c);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) { 
         Complex c1 = new Complex(10, 15); 
-        Complex c2 = new Complex(c1);     
+        Complex c2 = (Complex)new Complex(c1);     
         Complex c3 = c1;   
         System.out.println(c2); 
     } 
